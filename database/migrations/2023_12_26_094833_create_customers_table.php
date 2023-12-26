acfcd961a0b3a299e0ac4ce140f24b1c0319c522
mdrdani <sudokill3004@example.com>
2023-12-26 22:09:42 +0700

    Transaction Pages

--- database/migrations/2023_12_26_094833_create_customers_table.php
+++ database/migrations/2023_12_26_094833_create_customers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('no_telp');
+            $table->text('no_telp');
             $table->text('address');
             $table->timestamps();
         });
